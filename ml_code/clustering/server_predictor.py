c2af3f2c2308ed27af0ac79cb2a1339669e5cb04
rshrc <rishieric91@gmail.com>
2018-08-19 15:08:47 +0530

    Classifier and Regressor Model Attribute Specified

--- ml_code/clustering/server_predictor.py
+++ ml_code/clustering/server_predictor.py
@@ -1,2 +1,9 @@
-# -*- coding: utf-8 -*-
+from sklearn.externals import joblib
+import pickle
 
+
+classifier_model = joblib.load('classifier_model.pkl')
+regression_model = joblib.load('regression_model.pkl')
+
+clf = pickle.loads(classifier_model)
+reg = pickle.loads(regression_model)
